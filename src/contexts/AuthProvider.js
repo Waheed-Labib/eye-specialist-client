8c08d273d0dbf317f8f1da5eabb3382b9c57c930
Waheed-Labib <labibwahid28@gmail.com>
2023-09-01 11:26:46 +0600

    added firebase authentication methods in auth provider

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { FacebookAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import { app } from '../firebase/firabse.config';
 
 
@@ -8,8 +8,60 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
 
-    const authInfo = {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    const facebookProvider = new FacebookAuthProvider();
+
+    const createAccount = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const editProfile = profile => {
+        return updateProfile(auth.currentUser, profile)
+    }
+
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    const facebookSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, facebookProvider)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return () => unSubscribe()
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        setUser,
+        setLoading,
+        createAccount,
+        signIn,
+        editProfile,
+        resetPassword,
+        facebookSignIn,
+        logOut
     }
 
     return (
