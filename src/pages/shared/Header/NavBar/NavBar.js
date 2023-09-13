717168d3b3722b6b78cfb2012ead0ac737d86290
Waheed-Labib <labibwahid28@gmail.com>
2023-09-13 15:05:12 +0600

    sign out from navbar implemented

--- src/pages/shared/Header/NavBar/NavBar.js
+++ src/pages/shared/Header/NavBar/NavBar.js
@@ -2,102 +2,128 @@ import React, { useContext, useState } from 'react';
 import './NavBar.css'
 import greyLogo from '../../../../assets/images/logo/logo.png'
 import greenLogo from '../../../../assets/images/logo/green logo.png'
-import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../../contexts/AuthProvider';
-import { FaBookOpen, FaHome, FaNotesMedical, FaPencilAlt, FaRegListAlt, FaSignOutAlt, FaStar, FaUser } from 'react-icons/fa';
+import { FaHome, FaPen, FaPlusSquare, FaRegListAlt, FaSignInAlt, FaSignOutAlt, FaStar, FaUserAlt, FaUserPlus } from 'react-icons/fa';
+import toast from 'react-hot-toast';
 
 const NavBar = () => {
 
     const [logo, setLogo] = useState('greyLogo')
     const [menuOpen, setMenuOpen] = useState(false)
+    const { user, logOut } = useContext(AuthContext)
 
-    const { user } = useContext(AuthContext)
+    const handleSignOut = () => {
+        logOut()
+            .then(() => {
+                toast.success('Sign Out Successful')
+            })
+            .catch(() => {
+                toast.error('Something Went Wrong')
+            })
+    }
 
     return (
         <div className='navbar'>
-            <div className={`${menuOpen && 'hide-logo-section'}`}>
-                {/* logo */}
-                <Link to='/'>
+            {
+                logo === 'greyLogo' ?
                     <img
+                        className='logo'
                         onMouseOver={() => setLogo('greenLogo')}
-                        className={`logo ${logo === 'greenLogo' && 'd-none'}`} src={greyLogo} alt=''></img>
-                </Link>
-
-                {/* green logo */}
-                <Link to='/'>
+                        src={greyLogo}
+                        alt=''></img>
+                    :
                     <img
-                        onMouseLeave={() => setLogo('logo')}
-                        className={`logo ${logo !== 'greenLogo' && 'd-none'}`} src={greenLogo} alt=''></img>
-                </Link>
-            </div>
+                        className='logo'
+                        onMouseLeave={() => setLogo('greyLogo')}
+                        src={greenLogo}
+                        alt=''></img>
+            }
 
-            <div className='menu'>
+            <div className={`menu ${menuOpen && 'menu-open'}`}>
+                <div
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    className='toggle-menuOpen'>
 
-                <div onClick={() => setMenuOpen(!menuOpen)} className='view-menu'>
-                    <p className={`view-menu-text ${menuOpen && 'd-none'}`}>View Menu</p>
-                    <p className={`view-menu-text close ${!menuOpen && 'd-none'}`}>Close</p>
+                    <p className={`${menuOpen && 'd-none'}`}>View Menu</p>
+                    <p className={`${menuOpen || 'd-none'} close-text`}>Close</p>
 
                     <div className='lines'>
-                        <div className={`line ${menuOpen && 'rotate-line rotate-first-line'}`}></div>
-                        <div className={`line second-line ${menuOpen && 'rotate-line rotate-second-line'}`}></div>
+                        <div className={`line ${menuOpen ? 'rotated-first-line' : 'first-line'}`}></div>
+                        <div className={`line ${menuOpen ? 'rotated-second-line' : 'second-line'}`}></div>
                     </div>
-                </div>
 
+                </div>
 
                 {
                     menuOpen &&
-                    <div onClick={() => setMenuOpen(true)} className='menu-links'>
+                    <div className='menu-options'>
+
                         {
                             user &&
-                            <div>
-                                <div className='user-id'>
-                                    <FaUser></FaUser>
-                                    <p>
-                                        <small>
-                                            <span style={{ color: 'yellowgreen' }}>logged in</span>&nbsp;as&nbsp;
-                                        </small>
-                                        <span style={{ fontWeight: '700' }}>{user?.displayName}</span>
-                                    </p>
-                                </div>
-
-                                <Link to='/' className='menu-option'>
-                                    <FaHome></FaHome>
-                                    <p>Home</p>
-                                </Link>
-                                <hr></hr>
-                                <Link to='/services' className='menu-option'>
-                                    <FaNotesMedical></FaNotesMedical>
-                                    <p>Services</p>
-                                </Link>
-                                <hr></hr>
-                                <Link to={`/reviews/${user?.uid}`} className='menu-option'>
-                                    <FaStar style={{ color: 'orangered' }}></FaStar>
-                                    <p>My Reviews</p>
-                                </Link>
-                                <hr></hr>
-                                <Link to='/add-service' className='menu-option'>
-                                    <FaPencilAlt></FaPencilAlt>
-                                    <p>Add Service</p>
-                                </Link>
-                                <hr></hr>
-                                <Link className='menu-option'>
-                                    <FaSignOutAlt></FaSignOutAlt>
-                                    <p>Sign Out</p>
-                                </Link>
-                                <hr></hr>
-                                <Link to='/blogs' className='menu-option'>
-                                    <FaBookOpen></FaBookOpen>
-                                    <p>Blogs</p>
-                                </Link>
+                            <div className='user-info'>
+                                <FaUserAlt></FaUserAlt>
+                                <p>Logged in as <span style={{ fontSize: '1.1rem', fontWeight: '700' }}>{user?.displayName}</span></p>
                             </div>
                         }
+
+
+                        <div className='menu-option'>
+                            <FaHome></FaHome>
+                            <p>Home</p>
+                        </div>
+
+                        <div className='menu-option'>
+                            <FaRegListAlt></FaRegListAlt>
+                            <p>Services</p>
+                        </div>
+
+                        {
+                            user ?
+                                <>
+                                    <div className='menu-option'>
+                                        <FaStar></FaStar>
+                                        <p>My Reviews</p>
+                                    </div>
+
+                                    <div className='menu-option'>
+                                        <FaPlusSquare></FaPlusSquare>
+                                        <p>Add Service</p>
+                                    </div>
+
+                                </>
+                                :
+                                <>
+                                    <div className='menu-option'>
+                                        <FaSignInAlt></FaSignInAlt>
+                                        <p>Sign in</p>
+                                    </div>
+
+                                    <div className='menu-option'>
+                                        <FaUserPlus></FaUserPlus>
+                                        <p>Create Account</p>
+                                    </div>
+                                </>
+                        }
+
+                        <div className='menu-option'>
+                            <FaPen></FaPen>
+                            <p>Blog</p>
+                        </div>
+
+                        {
+                            user &&
+                            <div onClick={handleSignOut} className='menu-option'>
+                                <FaSignOutAlt></FaSignOutAlt>
+                                <p>Sign Out</p>
+                            </div>
+                        }
+
+
                     </div>
                 }
-
             </div>
 
         </div>
-
     );
 };
 
