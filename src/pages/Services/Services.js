3a468db85319244e16f31c6e107db244bf757dc3
Waheed-Labib <labibwahid28@gmail.com>
2023-10-22 13:34:49 +0600

    updated pagination in services page

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -9,27 +9,31 @@ const Services = () => {
 
     const [serviceLoading, setServiceLoading] = useState(false);
     const [services, setServices] = useState([]);
-    const [dataNotFound, setDataNotFound] = useState(false)
+    const [servicesCount, setServicesCount] = useState(0);
+    const [dataNotFound, setDataNotFound] = useState(false);
+
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         setServiceLoading(true);
 
-        fetch('https://eye-specialist-server.vercel.app/services')
+        fetch(`https://eye-specialist-server.vercel.app/services?page=${page - 1}&size=6`)
             .then(res => res.json())
             .then(data => {
                 setServices(data.services)
+                setServicesCount(data.count)
                 setServiceLoading(false)
             })
             .catch(() => {
                 setDataNotFound(true)
                 setServiceLoading(false)
             })
-    }, [])
+    }, [page])
 
     return (
         <div>
             <ServicesSection services={services} serviceLoading={serviceLoading} dataNotFound={dataNotFound}></ServicesSection>
-            <ServicesPagination></ServicesPagination>
+            <ServicesPagination page={page} setPage={setPage} dataCount={servicesCount}></ServicesPagination>
             <AddNewServiceLink></AddNewServiceLink>
         </div>
 
--- src/pages/Services/ServicesPagination/ServicesPagination.js
+++ src/pages/Services/ServicesPagination/ServicesPagination.js
@@ -2,20 +2,45 @@ import React from 'react';
 import './ServicesPagination.css'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
-const ServicesPagination = () => {
+const ServicesPagination = ({ page, setPage, dataCount }) => {
+
+    // data per page = 6
+    const pageCount = Math.ceil(dataCount / 6);
+
+    const handleGoToPreviousPage = () => {
+        if (page > 1) {
+            setPage(page - 1);
+            if (window.innerWidth < 768) window.scrollTo(0, 300)
+            else if (window.innerWidth < 960) window.scrollTo(0, 500)
+            else if (window.innerWidth < 1124) window.scrollTo(0, 600)
+            else window.scrollTo(0, 700)
+        }
+    }
+
+    const handleGoToNextPage = () => {
+        if (page < pageCount) {
+            setPage(page + 1);
+            if (window.innerWidth < 768) window.scrollTo(0, 300)
+            else if (window.innerWidth < 960) window.scrollTo(0, 500)
+            else if (window.innerWidth < 1124) window.scrollTo(0, 600)
+            else window.scrollTo(0, 700)
+        }
+    }
+
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '25px', marginTop: '0', marginBottom: '7.5%' }}>
-            <button className='pagination-btn'>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '25px', marginTop: '5%', marginBottom: '7.5%' }}>
+            <button onClick={handleGoToPreviousPage} className={`${page !== 1 ? 'pagination-btn' : 'disabled-pagination-btn'}`}>
                 <FaAngleLeft></FaAngleLeft>
             </button>
 
             <p>Page &nbsp;
-                <span style={{ fontSize: '1.2rem', fontWeight: '600' }}>2</span>
+                <span style={{ fontSize: '1.1rem', fontWeight: '600' }}>{page}</span>
                 &nbsp; of &nbsp;
-                <span style={{ fontSize: '1.2rem', fontWeight: '600' }}>4</span>
+                <span style={{ fontSize: '1.1rem', fontWeight: '600' }}>{pageCount}</span>
             </p>
 
-            <button className='pagination-btn'>
+            <button onClick={handleGoToNextPage} className={`${page !== pageCount ? 'pagination-btn' : 'disabled-pagination-btn'}`}>
                 <FaAngleRight></FaAngleRight>
             </button>
         </div>
